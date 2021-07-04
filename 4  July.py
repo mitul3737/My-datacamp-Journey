7e059583e73958a558d3940c34c4706cbe3534b1
Md Shahriyar Al Mustakim Mitul <shahriyarmitul3737@gmail.com>
2021-07-04 18:57:30 +0600

    Datacamp

---
+++ 4  July.py
@@ -0,0 +1,85 @@
+Extend a list
+If you can change elements in a list, you sure want to be able to add elements to it, right You can use the + operator
+x = [a, b, c, d]
+y = x + [e, f]
+You just won the lottery, awesome! You decide to build a poolhouse and a garage. Can you add the information to the areas list
+•	Use the + operator to paste the list [poolhouse, 24.5] to the end of the areas list. Store the resulting list as areas_1.
+•	Further extend areas_1 by adding data on your garage. Add the string garage and float 15.45. Name the resulting list areas_2.
+•	# Create the areas list and make some changes
+•	areas = [hallway, 11.25, kitchen, 18.0, chill zone, 20.0,
+•	         bedroom, 10.75, bathroom, 10.50]
+•	
+•	# Add poolhouse data to areas, new list is areas_1
+•	
+•	# Add garage data to areas_1, new list is areas_2
+•	
+Answer
+# Create the areas list and make some changes
+areas = [hallway, 11.25, kitchen, 18.0, chill zone, 20.0,
+         bedroom, 10.75, bathroom, 10.50]
+
+# Add poolhouse data to areas, new list is areas_1
+areas_1=areas+[poolhouse, 24.5]
+
+# Add garage data to areas_1, new list is areas_2
+areas_2=areas_1+[garage,15.45]
+
+Delete list elements
+Finally, you can also remove elements from your list. You can do this with the del statement
+x = [a, b, c, d]
+del(x[1])
+Pay attention here as soon as you remove an element from a list, the indexes of the elements that come after the deleted element all change!
+The updated and extended version of areas that you've built in the previous exercises is coded below. You can copy and paste this into the IPython Shell to play around with the result.
+areas = [hallway, 11.25, kitchen, 18.0,
+        chill zone, 20.0, bedroom, 10.75,
+         bathroom, 10.50, poolhouse, 24.5,
+         garage, 15.45]
+There was a mistake! The amount you won with the lottery is not that big after all and it looks like the poolhouse isn't going to happen. You decide to remove the corresponding string and float from the areas list.
+The ; sign is used to place commands on the same line. The following two code chunks are equivalent
+# Same line
+command1; command2
+
+# Separate lines
+command1
+command2
+Which of the code chunks will do the job for us
+Possible Answers
+•	 
+del(areas[10]); del(areas[11])
+•	 
+del(areas[1011])
+•	 
+del(areas[-4-2])
+•	 
+del(areas[-3]); del(areas[-4])
+
+Inner workings of lists
+At the end of the video, Hugo explained how Python lists work behind the scenes. In this exercise you'll get some hands-on experience with this.
+The Python code in the script already creates a list with the name areas and a copy named areas_copy. Next, the first element in the areas_copy list is changed and the areas list is printed out. If you hit Run Code you'll see that, although you've changed areas_copy, the change also takes effect in the areas list. That's because areas and areas_copy point to the same list.
+If you want to prevent changes in areas_copy from also taking effect in areas, you'll have to do a more explicit copy of the areas list. You can do this with list() or by using [].
+Change the second command, that creates the variable areas_copy, such that areas_copy is an explicit copy of areas. After your edit, changes made to areas_copy shouldn't affect areas. Submit the answer to check this.
+# Create list areas
+areas = [11.25, 18.0, 20.0, 10.75, 9.50]
+
+# Create areas_copy
+areas_copy = areas
+
+# Change areas_copy
+areas_copy[0] = 5.0
+
+# Print areas
+print(areas)
+
+Solve
+# Create list areas
+areas = [11.25, 18.0, 20.0, 10.75, 9.50]
+
+# Create areas_copy
+areas_copy = areas[]
+
+# Change areas_copy
+areas_copy[0] = 5.0
+
+# Print areas
+print(areas)
+
